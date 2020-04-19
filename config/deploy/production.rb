e2ee30fb2d7dece76085e84875e7a0b33c942d0d
susia9610 <61152544+susia9610@users.noreply.github.com>
2020-04-19 18:57:31 +0900

    add basic_auth

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 server '18.181.15.249', user: 'ec2-user', roles: %w{app db web}
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
